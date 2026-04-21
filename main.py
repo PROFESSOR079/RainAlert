aa4d46d94a2d9dd7fea5d2bf0c98c1b1e422d208
Gugalomi=geo <iva290386@gmail.com>
2026-04-21 21:06:50 +0400

    Check if it will rain in next 12 hours

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
 
-MY_LAT = 41.715137
-MY_LNG = 44.827095
+MY_LAT = 58.602901
+MY_LNG = 49.668060
 OWM_Endpoint = "https://api.openweathermap.org/data/2.5/forecast"
 api_key = "#############################"
 
@@ -9,8 +9,19 @@ parameters = {
     "appid": api_key,
     "lat": MY_LAT,
     "lon": MY_LNG,
+    "cnt": 4,
 }
 
 response = requests.get(OWM_Endpoint, params=parameters)
 response.raise_for_status()
-data = response.json()
+weather_data = response.json()
+
+will_rain = False
+for data in weather_data["list"]:
+    condition_code = data["weather"][0]["id"]
+
+    if int(condition_code) < 700:
+        will_rain = True
+
+if will_rain:
+    print("Bring an umbrella")
